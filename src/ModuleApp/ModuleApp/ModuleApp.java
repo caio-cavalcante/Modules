ec928eb3d52c671608f6d5b63ac8616a8fdada01
caio <caio.cav.ara@gmail.com>
2024-09-24 16:46:11 -0300

    typo

---
+++ src/ModuleApp/ModuleApp/ModuleApp.java
@@ -0,0 +1,8 @@
+package ModuleApp;
+import ModuleSaudacao.Saudacao;
+
+public class ModuleApp {
+    public static void main(String[] args) {
+        System.out.println(new Saudacao().saudar("World"));
+    }
+}
